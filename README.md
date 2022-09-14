68972f265eb70a78c37b0f78857bc87edfb19c0e
ztaylor54 <ztaylor54@gmail.com>
2022-09-14 11:42:15 +0200

    Move some static images around, update readme

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Computer-Aided Die Study (CADS)
-This project is a work-in-progress for my undergraduate thesis at Trinity University, in collaboration with the American Numismatic Society.
+
+<img src="./static/imgs/header.png" width="600">
+
+<!-- ![header image](./static/imgs/header.png "Header") -->
+
+The Computer-Aided Die Study (CADS) is a new way to conduct die studies using computer vision techniques. This project is a continuation and re-imagining of [previous efforts](https://numismatics.org/pocketchange/cads/) by the American Numismatic Society to create a computational die study program, with the added intention of producing a user-friendly tool numismatists can use for their research.
+
+The primary goal of CADS is to vastly reduce the time required for numismatists to conduct die studies. Die studies are traditionally conducted by hand, and are one of the most arduous tasks a numismatist can undertake. With CADS, the time required can be reduced from months-to-years to only a few hours of work.
+
+This project has been developed in collaboration with the [American Numismatic Society](https://numismatics.org/). 
+
+You can view a talk on CADS [here](https://youtu.be/wF6aZdhc0wg), or read about it [here](https://digitalcommons.trinity.edu/compsci_honors/54).
 
 ## Building
+
 A pre-compiled version of `opencv.js` is included out-of-the-box, but you can use `dev/build_opencvjs_linux.sh` to compile it yourself on a linux machine. It will install the necessary toolchain. Be sure to have Python 2.7.x installed before running.
 
 ## Running
+
 To run the application from source, simply `npm install` to get the dependencies, then `npm start`. This will bring up an Electron window.
--- js/clustering.js
+++ js/clustering.js
@@ -1,14 +1,14 @@
-const cv = require('opencv4nodejs');
-const ih = require('../js/modules/image-helpers.js');
-const fs = require('fs');
-const path = require('path');
-const { agnes } = require('ml-hclust');
-const dendrogram = require('../js/modules/dendrogram.js');
-const modal = require('../js/modules/modal.js');
+const cv = require("opencv4nodejs");
+const ih = require("../js/modules/image-helpers.js");
+const fs = require("fs");
+const path = require("path");
+const { agnes } = require("ml-hclust");
+const dendrogram = require("../js/modules/dendrogram.js");
+const modal = require("../js/modules/modal.js");
 // const remote = require('electron').remote;
-const tmp = require('tmp');
-const mdc = require('material-components-web');
-const Tokenfield = require('tokenfield');
+const tmp = require("tmp");
+const mdc = require("material-components-web");
+const Tokenfield = require("tokenfield");
 
 // Root dir for input images
 var rootDir;
@@ -39,9 +39,10 @@ function loadFromFile(fileInput) {
   } catch (e) {
     // Notify the user of a failed load
     var MDCSnackbar = mdc.snackbar.MDCSnackbar;
-    var snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-    const snackbar_label = document.querySelector('.mdc-snackbar__label');
-    snackbar_label.innerHTML = "Unable to load file: not a valid CADS data file.";
+    var snackbar = new MDCSnackbar(document.querySelector(".mdc-snackbar"));
+    const snackbar_label = document.querySelector(".mdc-snackbar__label");
+    snackbar_label.innerHTML =
+      "Unable to load file: not a valid CADS data file.";
 
     console.log(e);
 
@@ -52,17 +53,16 @@ function loadFromFile(fileInput) {
 /* Called after userDefineFeatureRadius modal is closed */
 function extractFeatures() {
   // Will have been defined by userDefineFeatureRadius modal
-  const featureRadius = remote.getGlobal('shared').featureRadius;
+  const featureRadius = remote.getGlobal("shared").featureRadius;
 
   console.log("featureRadius", featureRadius);
 
   // Time this process
   var start = Date.now();
-  console.log("Starting timer...")
+  console.log("Starting timer...");
 
   // Read dir input and prepare images for processing
-  fs.readdir(rootDir, function(err, files) {
-
+  fs.readdir(rootDir, function (err, files) {
     // Make progress bar visible
     // TODO: Refactor so the renderer can render the progress change
     // progInit(files.length);
@@ -72,16 +72,16 @@ function extractFeatures() {
       const cvMats = preProcess(file);
       const desc = cvMats.descriptors;
       const kps = cvMats.keypoints;
-      descriptors.push({'filename': file, 'descriptors': desc, 'keypoints': kps});
+      descriptors.push({ filename: file, descriptors: desc, keypoints: kps });
       console.log(index);
       // updateProgBar(index, files.length);
     });
 
     // Stop timing
     var millis = Date.now() - start;
-    console.log("Seconds elapsed = " + Math.floor(millis/1000));
+    console.log("Seconds elapsed = " + Math.floor(millis / 1000));
 
-    //makeImages();
+    // makeImages(); // debug to show single orb feature match
 
     // Display the graph
     cluster();
@@ -93,88 +93,85 @@ function makeImages() {
     // detect keypoints
     const keyPoints1 = detector.detect(img1);
     const keyPoints2 = detector.detect(img2);
-  
+
     // compute feature descriptors
     const descriptors1 = detector.compute(img1, keyPoints1);
     const descriptors2 = detector.compute(img2, keyPoints2);
-  
+
     // match the feature descriptors
     const matches = matchFunc(descriptors1, descriptors2);
-  
+
     // Ignore keypoints outside user-defined radius
-    const center1 = new cv.Point(img1.rows/2, img1.cols/2);
-    const center2 = new cv.Point(img2.rows/2, img2.cols/2);
+    const center1 = new cv.Point(img1.rows / 2, img1.cols / 2);
+    const center2 = new cv.Point(img2.rows / 2, img2.cols / 2);
     var radius = 100;
-  
+
     // Iterate through all keypoints & omit if distance from center is too great
-    const filteredKeyPoints1 = keyPoints1.filter(kp => {
+    const filteredKeyPoints1 = keyPoints1.filter((kp) => {
       // Is this keypoint within the user-defined feature radius?
-      return center1.sub(kp.pt).norm() <= radius
+      return center1.sub(kp.pt).norm() <= radius;
     });
-  
-    const filteredKeyPoints2 = keyPoints2.filter(kp => {
+
+    const filteredKeyPoints2 = keyPoints2.filter((kp) => {
       // Is this keypoint within the user-defined feature radius?
-      return center2.sub(kp.pt).norm() <= radius
+      return center2.sub(kp.pt).norm() <= radius;
     });
-  
+
     // only keep good matches
     const bestN = 40;
-    const bestMatches = matches.sort(
-      (match1, match2) => match1.distance - match2.distance
-    ).slice(0, bestN);
+    const bestMatches = matches
+      .sort((match1, match2) => match1.distance - match2.distance)
+      .slice(0, bestN);
 
     const dist = Math.abs(
-      bestMatches.reduceRight(
-        (acc, curr) => acc + Math.abs(curr.distance),
-        0
-      ) / bestMatches.length
+      bestMatches.reduceRight((acc, curr) => acc + Math.abs(curr.distance), 0) /
+        bestMatches.length
     );
 
     console.log(dist);
 
-  
-    return cv.drawMatches(
-      img1,
-      img2,
-      keyPoints1,
-      keyPoints2,
-      bestMatches
-    );
+    return cv.drawMatches(img1, img2, keyPoints1, keyPoints2, bestMatches);
   };
-  
+
   // const img1 = cv.imread('../cads/resources/images for thesis/0081_D44.jpg');
   // const img2 = cv.imread('../cads/resources/images for thesis/0082_D44.jpg');
   // const img2 = cv.imread('../cads/resources/vset 3/0112_D55.jpg');
-  const img1 = cv.imread('../cads/resources/vset 3/0094_D52.jpg');
-  const img2 = cv.imread('../cads/resources/vset 3/0098_D51.jpg');
-  
+  const img1 = cv.imread("../cads/resources/vset 3/0094_D52.jpg");
+  const img2 = cv.imread("../cads/resources/vset 3/0098_D51.jpg");
+
   const orbMatchesImg = matchFeatures({
     img1,
     img2,
     detector: new cv.ORBDetector(),
-    matchFunc: cv.matchBruteForceHamming
+    matchFunc: cv.matchBruteForceHamming,
   });
-  cv.imshowWait('ORB matches', orbMatchesImg);
+  cv.imshowWait("ORB matches", orbMatchesImg);
 }
 
 /* Prompts the user to define the feature radius */
 function userDefineFeatureRadius() {
-  var url = 'file://' + __dirname + '/modal/defineFeatureRadius.html';
+  var url = "file://" + __dirname + "/modal/defineFeatureRadius.html";
 
   // Set image path for modal to read
   console.log(path.join(rootDir, fs.readdirSync(rootDir)[0]));
-  remote.getGlobal('shared').featureRadiusImage = path.join(rootDir, fs.readdirSync(rootDir)[0]);
+  remote.getGlobal("shared").featureRadiusImage = path.join(
+    rootDir,
+    fs.readdirSync(rootDir)[0]
+  );
 
   modal.launchModal(url, 1000, 600, userDefineBlurRadius);
 }
 
 /* Prompts the user to define the ksize for the median blur */
 function userDefineBlurRadius() {
-  var url = 'file://' + __dirname + '/modal/defineBlurRadius.html';
+  var url = "file://" + __dirname + "/modal/defineBlurRadius.html";
 
   // Set image path for modal to read
   console.log(path.join(rootDir, fs.readdirSync(rootDir)[0]));
-  remote.getGlobal('shared').blurRadiusImage = path.join(rootDir, fs.readdirSync(rootDir)[0]);
+  remote.getGlobal("shared").blurRadiusImage = path.join(
+    rootDir,
+    fs.readdirSync(rootDir)[0]
+  );
 
   // Continue with feature extraction afterwards
   modal.launchModal(url, 1000, 600, extractFeatures);
@@ -182,7 +179,6 @@ function userDefineBlurRadius() {
 
 /* Extracts features from an image and returns the descriptors */
 function preProcess(file) {
-
   // Read the image to a mat
   var src = cv.imread(path.join(rootDir, file));
 
@@ -197,7 +193,7 @@ function preProcess(file) {
   const keyPoints = detector.detect(src);
 
   // Ignore keypoints outside user-defined radius
-  var center = new cv.Point(src.rows/2, src.cols/2);
+  var center = new cv.Point(src.rows / 2, src.cols / 2);
   var radius = getFeatureRadius();
 
   /*
@@ -209,9 +205,9 @@ function preProcess(file) {
   */
 
   // Iterate through all keypoints & omit if distance from center is too great
-  var filteredKeyPoints = keyPoints.filter(kp => {
+  var filteredKeyPoints = keyPoints.filter((kp) => {
     // Is this keypoint within the user-defined feature radius?
-    return center.sub(kp.pt).norm() <= radius
+    return center.sub(kp.pt).norm() <= radius;
   });
 
   /*
@@ -225,7 +221,7 @@ function preProcess(file) {
   // Return the descriptors
   return {
     descriptors: detector.compute(src, keyPoints),
-    keypoints: keyPoints
+    keypoints: keyPoints,
   };
 }
 
@@ -236,13 +232,13 @@ function cluster() {
 
   // Time this process
   var start = Date.now();
-  console.log("Starting timer for hierarchical clustering...")
+  console.log("Starting timer for hierarchical clustering...");
 
   // hierarchical clustering clustering
   const tree = agnes(descriptors, {
-    method: 'ward',
-    distanceFunction: distance
-  })
+    method: "ward",
+    distanceFunction: distance,
+  });
 
   console.log(tree);
 
@@ -256,7 +252,7 @@ function cluster() {
 
   // Stop timing
   var millis = Date.now() - start;
-  console.log("Seconds elapsed = " + Math.floor(millis/1000));
+  console.log("Seconds elapsed = " + Math.floor(millis / 1000));
 }
 
 /* Custom distance function for clustering */
@@ -269,9 +265,9 @@ function distance(objA, objB) {
 
   // Filter out the best 20 matches
   const bestN = 20;
-  const bestMatches = matches.sort(
-    (match1, match2) => match1.distance - match2.distance
-  ).slice(0, bestN);
+  const bestMatches = matches
+    .sort((match1, match2) => match1.distance - match2.distance)
+    .slice(0, bestN);
 
   // // Debug to show the feature matches
   // const matchesMat = cv.drawMatches(
@@ -285,10 +281,8 @@ function distance(objA, objB) {
 
   // Return the average distance between matched descriptors
   let dist = Math.abs(
-    bestMatches.reduceRight(
-      (acc, curr) => acc + Math.abs(curr.distance),
-      0
-    ) / bestMatches.length
+    bestMatches.reduceRight((acc, curr) => acc + Math.abs(curr.distance), 0) /
+      bestMatches.length
   );
 
   //console.log("dist: " + dist);
@@ -300,12 +294,13 @@ function distance(objA, objB) {
 /* Annotate a cluster */
 function annotate(cluster) {
   // Base case
-  if(cluster.isLeaf) {
+  if (cluster.isLeaf) {
     return [cluster.index];
-  }
-  else {
+  } else {
     // Annotate this node and pass along
-    cluster.members = cluster.children.map(x => annotate(x)).reduce((curr, acc) => acc.concat(curr), []);
+    cluster.members = cluster.children
+      .map((x) => annotate(x))
+      .reduce((curr, acc) => acc.concat(curr), []);
     return cluster.members;
   }
 }
@@ -316,7 +311,7 @@ function clearDir(dirname) {
     if (err) throw err;
 
     for (const file of files) {
-      fs.unlink(path.join(dirname, file), err => {
+      fs.unlink(path.join(dirname, file), (err) => {
         if (err) throw err;
       });
     }
@@ -327,19 +322,19 @@ function onOpenCvReady() {
   //document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
 }
 function onOpenCvError() {
-  let element = document.getElementById('status');
-  element.setAttribute('class', 'err');
-  element.innerHTML = 'Failed to load opencv.js';
+  let element = document.getElementById("status");
+  element.setAttribute("class", "err");
+  element.innerHTML = "Failed to load opencv.js";
 }
 
 /* Get the user-defined feature radius from its global container */
 function getFeatureRadius() {
-  return remote.getGlobal('shared').featureRadius;
+  return remote.getGlobal("shared").featureRadius;
 }
 
 /* Get the user-defined blur radius from its global container */
 function getBlurRadius() {
-  return remote.getGlobal('shared').blurRadius;
+  return remote.getGlobal("shared").blurRadius;
 }
 
 function progInit(max) {
@@ -358,12 +353,12 @@ function updateProgBar(value, max) {
   progBar.max = max;
 
   var progInfo = document.getElementById("progInfo");
-  progInfo.innerHTML = value + "/" + max + " (" + (value / max) + "%)";
+  progInfo.innerHTML = value + "/" + max + " (" + value / max + "%)";
 }
 
 /* Clean up dendrogram div */
 function cleanup() {
-  const d = document.getElementById('dendrogram');
+  const d = document.getElementById("dendrogram");
   while (d.firstChild) {
     d.removeChild(d.firstChild);
   }
@@ -377,7 +372,7 @@ function radiusChanged(newVal) {
   drawCircleAndDisplay(imgPath, newVal);
 
   // Set this value globally
-  remote.getGlobal('shared').featureRadius = newVal;
+  remote.getGlobal("shared").featureRadius = newVal;
 }
 
 /* UI ACTIONS THAT INTERACT WITH OPENCV OR CLUSTERING DATA STRUCTURE
@@ -388,10 +383,10 @@ function radiusChanged(newVal) {
  */
 
 // Launch modals on nav action
-const navNewEl = document.querySelector('.nav-action-new');
-navNewEl.addEventListener('click', (event) => {
+const navNewEl = document.querySelector(".nav-action-new");
+navNewEl.addEventListener("click", (event) => {
   // Create an input element to get dir input from user
-  var input = document.createElement('input');
+  var input = document.createElement("input");
   input.setAttribute("type", "file");
   input.setAttribute("id", "dirInput");
   input.setAttribute("accept", "image/*");
@@ -402,21 +397,20 @@ navNewEl.addEventListener('click', (event) => {
   return false;
 });
 
-const navLoadEl = document.querySelector('.nav-action-open');
-navLoadEl.addEventListener('click', (event) => {
+const navLoadEl = document.querySelector(".nav-action-open");
+navLoadEl.addEventListener("click", (event) => {
   // Create an input element to get json input from user
-  var input = document.createElement('input');
+  var input = document.createElement("input");
   input.setAttribute("type", "file");
   input.setAttribute("accept", ".json");
   input.setAttribute("onchange", "loadFromFile(this)");
   input.click();
   return false;
-
 });
 
 // Handle tags to dies
 const tf = new Tokenfield({
-  el: document.querySelector('.props-tags-input'), // Attach Tokenfield to the input element with class "text-input"
+  el: document.querySelector(".props-tags-input"), // Attach Tokenfield to the input element with class "text-input"
   items: [
     // Autocomplete items go here
   ],
@@ -424,47 +418,49 @@ const tf = new Tokenfield({
     // Pre-set items go here
   ],
   newItems: true,
-  placeholder: "Enter a tag..."
+  placeholder: "Enter a tag...",
 });
 
 /* Serialize the OpenCV state to the global scope */
 function save() {
-  var pjson = require('../package.json');
+  var pjson = require("../package.json");
 
   var saveFile = {
     meta: {
-      version: pjson.version
+      version: pjson.version,
     },
     rootDir: rootDir,
     clustering: globalTree,
-    tags: remote.getGlobal('shared').tags,
-    descriptors: []
+    tags: remote.getGlobal("shared").tags,
+    descriptors: [],
   };
 
   // Encode the descriptors and add them to the save file
-  descriptors.forEach(desc => {
-    const descOutBase64 = cv.imencode('.jpg', desc.descriptors).toString('base64'); // Perform base64 encoding
+  descriptors.forEach((desc) => {
+    const descOutBase64 = cv
+      .imencode(".jpg", desc.descriptors)
+      .toString("base64"); // Perform base64 encoding
     saveFile.descriptors.push({
       filename: desc.filename,
       descriptors: descOutBase64,
-      keypoints: desc.keypoints
-    })
+      keypoints: desc.keypoints,
+    });
   });
 
   // Write to temp file
   tmp.file(function _tempFileCreated(err, path, fd, cleanupCallback) {
     if (err) throw err;
 
-    console.log('File: ', path);
-    console.log('Filedescriptor: ', fd);
+    console.log("File: ", path);
+    console.log("Filedescriptor: ", fd);
 
-    fs.writeFile(path, JSON.stringify(saveFile), 'utf8', function(err) {
-      if(err) {
+    fs.writeFile(path, JSON.stringify(saveFile), "utf8", function (err) {
+      if (err) {
         return console.log(err);
       }
-        console.log("Saved temp file!");
+      console.log("Saved temp file!");
 
-        remote.getGlobal('shared').tempFileLoc = path;
+      remote.getGlobal("shared").tempFileLoc = path;
     });
   });
 }
@@ -472,7 +468,7 @@ function save() {
 /* Load from serialized state */
 function load(saveFile) {
   // Read the file, then parse the fields
-  const data = fs.readFileSync(saveFile, 'utf8');
+  const data = fs.readFileSync(saveFile, "utf8");
 
   // Will throw error to parent if not valid JSON
   const parsedJSON = JSON.parse(data);
@@ -485,41 +481,45 @@ function load(saveFile) {
     throw new Error("No clustering data specified in save file, aborting load");
   }
 
-  if(!parsedJSON.descriptors) {
+  if (!parsedJSON.descriptors) {
     throw new Error("No descriptor data specified in save file, aborting load");
   }
 
-  if(!parsedJSON.tags) {
+  if (!parsedJSON.tags) {
     throw new Error("No tags data specified in save file, aborting load");
   }
 
   // Copy the tags over
   this.tags = new Array();
   var self = this;
-  parsedJSON.tags.forEach(tag => {
+  parsedJSON.tags.forEach((tag) => {
     self.tags.push(tag);
   });
 
-  remote.getGlobal('shared').tags = this.tags;
+  remote.getGlobal("shared").tags = this.tags;
 
   const descr = new Array();
 
-  parsedJSON.descriptors.forEach(desc => {
+  parsedJSON.descriptors.forEach((desc) => {
     // Convert to OpenCV mat
-    const descBuffer = Buffer.from(desc.descriptors,'base64');
-    const kpsBuffer = Buffer.from(desc.keypoints, 'base64');
+    const descBuffer = Buffer.from(desc.descriptors, "base64");
+    const kpsBuffer = Buffer.from(desc.keypoints, "base64");
     const descMat = cv.imdecode(descBuffer);
     const kpsMat = cv.imdecode(kpsBuffer);
 
     descr.push({
       filename: desc.filename,
       descriptors: descMat,
-      keypoints: kpsMat
+      keypoints: kpsMat,
     });
-
   });
 
   // Draw the dendrogram
-  dendrogram.drawDendrogram("#dendrogram", parsedJSON.clustering, descr, parsedJSON.rootDir);
+  dendrogram.drawDendrogram(
+    "#dendrogram",
+    parsedJSON.clustering,
+    descr,
+    parsedJSON.rootDir
+  );
 }
-const remote = require('electron').remote;
+const remote = require("electron").remote;
